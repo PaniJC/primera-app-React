f56f2184ecf8671389cfdc44375f05e401816cf3
codepaniJC <juan77camilo@gmail.com>
2021-07-05 14:16:51 -0500

    nuevo-cambio-app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Juan Camilo y Jeronimo
+          Learn React Cajerael
         </a>
       </header>
     </div>
