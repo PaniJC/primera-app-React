d290da8416492c7ba2bbff99767212a48b480cab
codepaniJC <juan77camilo@gmail.com>
2021-07-05 14:24:16 -0500

    prueba2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Cajerael
+          Learn React Cajerael!
         </a>
       </header>
     </div>
