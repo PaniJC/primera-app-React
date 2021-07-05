32ca89dc7705e1265ee53323fc303f32eee09300
codepaniJC <juan77camilo@gmail.com>
2021-07-05 14:06:26 -0500

    Mi primera app en React

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Juan Camilo y Jeronimo
         </a>
       </header>
     </div>
