f09694f9edf2ef7f974793378e13f7eceabd69c2
codepaniJC <juan77camilo@gmail.com>
2021-07-08 20:13:37 -0500

    Primer Formulario - Eventos onChange y onSubmit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState} from "react";
+//El componente es reactiuvo porque se está llamando una y otra vez y está actualizando lo que tiene que actualizar
+function App() { // Este es un componente funcional y no de clase
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-function App() {
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = ({ target : { value }}) => { //Otra forma de hacerlo
+    setPassword(value);
+  };
+
+  const handleFormSubmit = (event) => { //Otra forma de hacerlo
+    event.preventDefault();
+    alert(`Usuario: ${email}, Password: ${password}`);
+  };
+
+  console.log(email);
+  console.log(password);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Cajerael!
-        </a>
-      </header>
+      <form onSubmit={handleFormSubmit}>
+        <h2>Inciar sesión</h2>
+        <label>
+          Correo
+          <input type="email" value={email} onChange = {handleEmailChange}/>
+        </label>
+        <label>
+          Contraseña
+          <input type="password" value={password} onChange = {handlePasswordChange}/>
+        </label>
+        <button type="submit">
+          Enter
+        </button>
+      </form>
     </div>
   );
 }
